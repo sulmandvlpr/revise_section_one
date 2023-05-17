daf25ef65f010956121d4aba3ff1a8d5fb4bb453
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-17 14:30:57 +0500

    Cmt file 1/12 function defination and button design  Br1Prac1Lac1

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,10 +3,17 @@ import 'package:flutter/material.dart';
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
+//fun are also pass as values in flutter
+// we define func on top so we can easy change thing without deep dive in to widget tree
+// later when working with big project
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
+
+//define func after construction function and instace variables
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -20,11 +27,21 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-1.png',
               width: 200,
             ),
+            const SizedBox(height: 20),
+            TextButton(
+                onPressed: rollDice,
+                //.styleFrom()fun return ButtonStyle and this ButtonStyle is also return of style arg
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28),
+                ),
+                child: const Text('Roll Dice')),
           ],
         ),
       ),
