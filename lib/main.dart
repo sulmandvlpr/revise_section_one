c951a77a7fb91c1d922bab8db17aebd94b3c0720
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-20 00:49:59 +0500

    Cmt file 2/3 QuizApp Widget as Variable      Br1Prac1Lac1

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:revise_section_one/start_screen.dart';
-
-import 'package:revise_section_one/gradient_container.dart';
+import 'package:revise_section_one/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 109, 46, 218),
-                  Color.fromARGB(255, 211, 71, 236)
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen()),
-      ),
-    ),
+    const Quiz(),
   );
 }
