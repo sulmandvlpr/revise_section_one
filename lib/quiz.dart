86da316b5488b45f549220553b029fd58a8f36db
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-23 14:01:55 +0500

    Cmt file 2/3 QuizApp Widget as Variable      Br1Prac1Lac1

---
+++ lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart ';
+import 'package:revise_section_one/questions_screen.dart';
+
+import 'package:revise_section_one/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  // we can also Store Widget in Variable
+  // we can replace var with Widget to use any Widget type
+  // i am getting this error
+  Widget activeScreen = const StartScreen(switchScreen);
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 109, 46, 218),
+                  Color.fromARGB(255, 211, 71, 236)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: activeScreen),
+      ),
+    );
+  }
+}
