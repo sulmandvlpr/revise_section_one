f791984a63c3af09fab3fefcda4d35973617bac5
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-25 00:13:55 +0500

    Cmt file 2/6 model,data classes, dummy data QuizApp BrPrac2Lac2

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,10 @@
+//modele class
+//thnigs define here used in data class
+class QuizQuestion {
+  //construtor fun
+  const QuizQuestion(this.text, this.answers);
+
+  //2 class variable / instance variabls
+  final String text;
+  final List<String> answers;
+}
