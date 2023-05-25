18f034d0ce3e79b268cc87ac077d68a3af8723a8
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-25 11:34:03 +0500

    Cmt file 2/7 Elivated Button as Widget QuizApp BrPrac2Lac2

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:revise_section_one/answers_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,20 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Some text');
+    //colum take full height but the width left behind so to take full width also
+    //we need double.infinity of width property with sizedbox widget
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('the question text'),
+          const SizedBox(height: 20),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
