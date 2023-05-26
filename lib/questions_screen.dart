3ca784ea94a8a2e299bf640fcc8b99b0310265b5
sulmandvlpr <sulmanzafa@gmail.com>
2023-05-26 10:08:21 +0500

    Cmt file 2/10 button layout set same shape and size of button with diffrent data QuizApp BrPrac2Lac2

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -21,25 +21,30 @@ class _QuestionScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            //access the Property of questions List class by classname.prperty type
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          // ...currentQuestion.answers.map((answers) => AnswerButton(onTap: (){}, answerText: answers))
-          //set button text dynamically based on answers list
-          //so we need to convert answers list to widget answer list
-          //we convert list to widget list by applying .map((list element){}) method to list  which returns new Widget List
-          //.map return New Widget list but column have childer list [] so we use ...
-          //... spereding will take all value from list and pull them out from list and save them in comma seprated values
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(onTap: () {}, answerText: answer);
-          }),
-        ],
+      //to give dynamic text button fix shape wrape them with container and padding them
+      child: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              //access the Property of questions List class by classname.prperty type
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            // ...currentQuestion.answers.map((answers) => AnswerButton(onTap: (){}, answerText: answers))
+            //set button text dynamically based on answers list
+            //so we need to convert answers list to widget answer list
+            //we convert list to widget list by applying .map((list element){}) method to list  which returns new Widget List
+            //.map return New Widget list but column have childer list [] so we use ...
+            //... spereding will take all value from list and pull them out from list and save them in comma seprated values
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(onTap: () {}, answerText: answer);
+            }),
+          ],
+        ),
       ),
     );
   }
